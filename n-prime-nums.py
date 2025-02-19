82154692c9dd472d20a2d7c780bd83331044b731
jordand2003 <jordandominguez600@gmail.com>
2025-02-18 20:17:51 -0500

    completed

--- n-prime-nums.py
+++ n-prime-nums.py
@@ -1,1 +1,25 @@
-print('Hello')
+import sys
+
+def n_prime_nums(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        is_prime = True
+        for prime in primes:
+            if num % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        num += 1
+    return primes
+
+n = sys.argv[1]
+try :
+    n = int(n)
+    if n < 1:
+        print("Please enter a number greater than 0.")
+    else:
+        print(n_prime_nums(n))
+except ValueError:
+    print("Please enter a valid number.")
